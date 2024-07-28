d88f7d1246e319bc746f84c2e6ed7efc5a6c75d8
Eyepan <iamteoggy@gmail.com>
2024-07-28 13:48:23 +0530

    mainfile cleanup

--- main.py
+++ main.py
@@ -19,35 +19,41 @@ def write_links_to_file(filename, links):
     with open(filename, 'w') as file:
         file.write('\n'.join(links))
 
-def main():
+def parse_arguments():
     parser = ArgumentParser(description="Download series episodes")
     parser.add_argument('show_link', type=str, help="URL of the show")
     parser.add_argument('--aria', action='store_true', help="Use aria2c for downloading")
     parser.add_argument('--bypass', action='store_true', help="Bypass the saved links file and rescrape all links manually")
     parser.add_argument('--dry-run', action='store_true', help="Just fetches the file links, doesn't download anything")
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    show_link = args.show_link
-    file_links = []
-
-    if os.path.isfile(LINKS_FILE) and not args.bypass:
+def fetch_links(show_link, bypass):
+    if os.path.isfile(LINKS_FILE) and not bypass:
         print("Found link files from a previous run. Skipping link scraping and moving directly to downloading")
-        file_links = read_links_from_file(LINKS_FILE)
+        return read_links_from_file(LINKS_FILE)
     else:
         season_links = handlers.extract_all_season_links_from_series(show_link)
+        file_links = []
         for season in season_links:
             links = handlers.extract_all_downloadable_links_from_season_link(season)
             file_links.extend(links)
         write_links_to_file(LINKS_FILE, file_links)
+        return file_links
 
-    if args.dry_run:
-        return
-
-    if args.aria:
+def download_files(file_links, use_aria):
+    if use_aria:
         handlers.download_file_with_aria2c(LINKS_FILE)
     else:
         for file in file_links:
             handlers.download_with_wget(file)
+
+def main():
+    args = parse_arguments()
+
+    file_links = fetch_links(args.show_link, args.bypass)
+
+    if not args.dry_run:
+        download_files(file_links, args.aria)
 
 if __name__ == "__main__":
     main()
