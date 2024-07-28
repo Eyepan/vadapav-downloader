cc02ebc83a4359dc97b0ba64c8e78ee49baa7e10
Eyepan <iamteoggy@gmail.com>
2024-07-28 14:26:34 +0530

    datafile-cleanup

--- handlers.py
+++ handlers.py
@@ -9,21 +9,25 @@ import subprocess
 
 BASE_URL = "https://vadapav.mov"
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def extract_episode(filename):
     # Regular expression pattern to match the season and episode
-    pattern = re.compile(r'S\d{2}E\d{2}')
+    pattern = re.compile(r"S\d{2}E\d{2}")
     match = pattern.search(filename)
     if match:
         return match.group()
     else:
         return None
 
+
 def extract_all_season_links_from_series(series_link: str):
     """
     Extracts all season links from the given series link.
-    
+
     :param series_link: URL of the series
     :return: List of season URLs
     :raises: requests.HTTPError if the request fails
@@ -32,16 +36,19 @@ def extract_all_season_links_from_series(series_link: str):
     logging.info(f"Extracting season links for: {series_link}")
     response.raise_for_status()
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    season_links = soup.find_all('a', class_='directory-entry')
+    soup = BeautifulSoup(response.content, "html.parser")
+    season_links = soup.find_all("a", class_="directory-entry")
     season_links.pop(0)
     logging.info(f"Found {len(season_links)} season links")
-    return [urljoin(BASE_URL, link.get('href')) for link in season_links]
+    return [urljoin(BASE_URL, link.get("href")) for link in season_links]
 
-def extract_all_downloadable_links_from_season_link(season_link: str) -> List[str]:
+
+def extract_all_downloadable_links_from_season_link(
+    season_link: str,
+) -> List[dict]:
     """
     Extracts all downloadable file links from the given season link.
-    
+
     :param season_link: URL of the season
     :return: List of file URLs
     :raises: requests.HTTPError if the request fails
@@ -50,35 +57,45 @@ def extract_all_downloadable_links_from_season_link(season_link: str) -> List[st
     logging.info(f"Extracting download links for: {season_link}")
     response.raise_for_status()
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    file_links = soup.find_all('a', class_='file-entry')
+    soup = BeautifulSoup(response.content, "html.parser")
+    file_links = soup.find_all("li")
+    file_links.pop(0)
     file_links.pop(0)
     logging.info(f"Found {len(file_links)} file links")
-    output = []
-    for link in file_links:
-        url = urljoin(BASE_URL, link.get('href'))
-        details = extract_episode(link.text)
-        output.append(f"{url} # {details}")
-    return output
+    return [
+        {
+            "url": urljoin(BASE_URL, link.find('a').get("href")),
+            "details": {
+                "episode": extract_episode(link.find('a').text),
+                "show": link.find('a').text.split(".")[0],
+                "filename": link.find('a').text,
+                "size": link.find('div', class_='size-div').text
+            },
+        }
+        for link in file_links
+    ]
+
 
 def download_with_wget(link):
     """
     Downloads a file using wget.
-    
+
     :param link: URL of the file to download
     """
     logging.info(f"Downloading with wget: {link}")
-    subprocess.run(['wget', '--content-disposition', '--trust-server-names', link])
+    subprocess.run(["wget", "--content-disposition", "--trust-server-names", link])
+
 
 def download_with_aria2c(link):
     """
     Downloads a file using aria2c.
-    
+
     :param link: URL of the file to download
     """
     logging.info(f"Downloading with aria2c: {link}")
-    subprocess.run(['aria2c', '-x4', link, '--file-allocation=none'])
+    subprocess.run(["aria2c", "-x4", link, "--file-allocation=none"])
+
 
 def download_file_with_aria2c(filename: str):
     logging.info(f"Downloading with aria2c: {filename}")
-    subprocess.run(['aria2c', '-x4', f'-i {filename}', '--file-allocation=none', '-j1'])
+    subprocess.run(["aria2c", "-x4", f"-i {filename}", "--file-allocation=none", "-j1"])
--- main.py
+++ main.py
@@ -1,59 +1,78 @@
+from typing import List
 import handlers
-import sys
 import logging
+import json
 import os
 from argparse import ArgumentParser
 
-LINKS_FILE = "all_links.txt"
+LINKS_FILE = "links.json"
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def read_links_from_file(filename):
-    logging.info(f"Reading links from {filename}")
-    with open(filename, 'r') as file:
-        return file.read().splitlines()
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
 
-def write_links_to_file(filename, links):
-    logging.info(f'Writing {len(links)} links to {filename}')
-    with open(filename, 'w') as file:
-        file.write('\n'.join(links))
+
+def write_links_to_file(filename, data):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
 
 def parse_arguments():
     parser = ArgumentParser(description="Download series episodes")
-    parser.add_argument('show_link', type=str, help="URL of the show")
-    parser.add_argument('--aria', action='store_true', help="Use aria2c for downloading")
-    parser.add_argument('--bypass', action='store_true', help="Bypass the saved links file and rescrape all links manually")
-    parser.add_argument('--dry-run', action='store_true', help="Just fetches the file links, doesn't download anything")
+    parser.add_argument("show_link", type=str, help="URL of the show")
+    parser.add_argument(
+        "--aria", action="store_true", help="Use aria2c for downloading"
+    )
+    parser.add_argument(
+        "--bypass",
+        action="store_true",
+        help="Bypass the saved links file and rescrape all links manually",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Just fetches the file links, doesn't download anything",
+    )
     return parser.parse_args()
+
 
 def fetch_links(show_link, bypass):
     if os.path.isfile(LINKS_FILE) and not bypass:
-        print("Found link files from a previous run. Skipping link scraping and moving directly to downloading")
+        print(
+            "Found link files from a previous run. Skipping link scraping and moving directly to downloading"
+        )
         return read_links_from_file(LINKS_FILE)
     else:
         season_links = handlers.extract_all_season_links_from_series(show_link)
-        file_links = []
+        data = []
         for season in season_links:
             links = handlers.extract_all_downloadable_links_from_season_link(season)
-            file_links.extend(links)
-        write_links_to_file(LINKS_FILE, file_links)
-        return file_links
+            data.extend(links)
+        write_links_to_file(LINKS_FILE, data)
+        return data
 
-def download_files(file_links, use_aria):
-    if use_aria:
-        handlers.download_file_with_aria2c(LINKS_FILE)
-    else:
-        for file in file_links:
-            handlers.download_with_wget(file)
+
+def download_files(data: List[dict], use_aria):
+    for file in data:
+        if use_aria:
+            handlers.download_with_aria2c(file["url"])
+        else:
+            handlers.download_with_wget(file["url"])
+
 
 def main():
     args = parse_arguments()
 
-    file_links = fetch_links(args.show_link, args.bypass)
+    data = fetch_links(args.show_link, args.bypass)
 
     if not args.dry_run:
-        download_files(file_links, args.aria)
+        download_files(data, args.aria)
+
 
 if __name__ == "__main__":
     main()
